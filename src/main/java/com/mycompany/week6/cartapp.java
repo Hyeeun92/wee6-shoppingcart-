08369c18c37732e28ec9b6cfa724a0a102277416
Hyeeun Lee <21545@student.dorset-college.ie>
2020-10-20 14:10:23 +0100

    myfirst

--- src/main/java/com/mycompany/week6/cartapp.java
+++ src/main/java/com/mycompany/week6/cartapp.java
@@ -12,6 +12,13 @@ package com.mycompany.week6;
 public class cartapp {
      public static void main(String[] args)
   {
+     ArrayList<String> items = new ArrayList<String>();
+     Double [] price = {0.60, 0.25};
+     int quantity = 0;
+     double sum = 0;
+     
+     items.add("apple");
+     items.add("orange");
      
   }
 }
